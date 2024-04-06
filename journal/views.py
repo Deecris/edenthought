d78887fb641a1f03425cba4707bd449c19e52931
Diana Cristiana <diana_cristiana@yahoo.com>
2024-04-06 13:16:53 +0300

    -m  first entry
    Initial commit
    
    Changes to be committed:
    	new file:   db.sqlite3
    	new file:   edenthought/__init__.py
    	new file:   edenthought/__pycache__/__init__.cpython-311.pyc
    	new file:   edenthought/__pycache__/settings.cpython-311.pyc
    	new file:   edenthought/__pycache__/urls.cpython-311.pyc
    	new file:   edenthought/__pycache__/wsgi.cpython-311.pyc
    	new file:   edenthought/asgi.py
    	new file:   edenthought/settings.py
    	new file:   edenthought/urls.py
    	new file:   edenthought/wsgi.py
    	new file:   journal/__init__.py
    	new file:   journal/__pycache__/__init__.cpython-311.pyc
    	new file:   journal/__pycache__/admin.cpython-311.pyc
    	new file:   journal/__pycache__/apps.cpython-311.pyc
    	new file:   journal/__pycache__/models.cpython-311.pyc
    	new file:   journal/__pycache__/urls.cpython-311.pyc
    	new file:   journal/__pycache__/views.cpython-311.pyc
    	new file:   journal/admin.py
    	new file:   journal/apps.py
    	new file:   journal/migrations/__init__.py
    	new file:   journal/migrations/__pycache__/__init__.cpython-311.pyc
    	new file:   journal/models.py
    	new file:   journal/templates/journal/dashboard.html
    	new file:   journal/templates/journal/index.html
    	new file:   journal/templates/journal/my-login.html
    	new file:   journal/templates/journal/register.html
    	new file:   journal/tests.py
    	new file:   journal/urls.py
    	new file:   journal/views.py
    	new file:   manage.py

---
+++ journal/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def homepage(request):
+    return render(request, 'journal/index.html')
+
+
+def register(request):
+    return render(request, 'journal/register.html')
+
+
+def my_login(request):
+   return render(request, 'journal/my-login.html')
+
+
+def dashboard(request):
+    return render(request, 'journal/dashboard.html')
